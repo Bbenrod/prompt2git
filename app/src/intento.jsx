375caacc0fbfee525eb37bcb991f5a2f7c0584f0
Juan Carlos Espinoza <jc_espinozarodriguez@hotmail.com>
2023-09-04 21:55:43 -0600

    Actualizacion de iconos y pequeño cambio al diseño, asi como a la paleta de colores

---
+++ app/src/intento.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<React.StrictMode>
+    	<App />
+  	</React.StrictMode>
+);
+
+export default function App() {
+  	const [input1Value, setInput1Value] = useState('');
+  	const [input2Value, setInput2Value] = useState('');
+  	const [areInputsSwapped, setAreInputsSwapped] = useState(false);
+
+	const handleConvertClick = () => {
+		const inputValue = areInputsSwapped ? input2Value : input1Value;
+		const convertedText = inputValue.replace(/[aeiouAEIOU]/g, 'x');
+    
+		if (areInputsSwapped) {
+			setInput1Value(convertedText);
+		} else {
+			setInput2Value(convertedText);
+		}
+  	};
+
+  	const handleTurnClick = () => {
+		setAreInputsSwapped(!areInputsSwapped);
+  	};
+
+	return (
+		<div className='flex items-center justify-center min-h-screen bg-black'>
+			<div className='h-52 w-full max-w-4xl p-2 bg-gray-900 rounded-lg shadow-md flex items-center justify-between'>
+				<div className='flex flex-col h-full bg-gray-800 rounded p-2 w-5/12'>
+					<label for='inputText' className='block font-medium dark:text-gray-200 mb-2'>Lenguaje humano</label>
+					<textarea className='w-full h-full py-2 px-3 rounded text-white bg-gray-600'	value={areInputsSwapped ? input2Value : input1Value}
+						onChange={(e) =>
+							areInputsSwapped
+								? setInput2Value(e.target.value)
+								: setInput1Value(e.target.value)
+						}/>
+					<button	className='h-1/5 w-1/3 text-white bg-blue-500 rounded mt-2 self-end' onClick={handleConvertClick}>Convert</button>
+				</div>
+
+				<button	className='w-10 h-10 mx-2 text-white bg-blue-500 border border-blue-500 rounded-full flex items-center justify-center' onClick={handleTurnClick}>
+					<span>➜</span>
+				</button>
+
+				<div className='flex flex-col h-full bg-gray-800 rounded p-2 w-5/12'>
+					<label for='inputText' className='block font-medium dark:text-gray-200 mb-2'>Git prompt</label>
+					<textarea className='w-full h-full py-2 px-3 rounded text-white bg-gray-700'	value={areInputsSwapped ? input2Value : input1Value}
+						onChange={(e) =>
+							areInputsSwapped
+								? setInput2Value(e.target.value)
+								: setInput1Value(e.target.value)
+						}/>
+					<button	className='h-1/5 w-1/3 text-white bg-blue-500 rounded mt-2' onClick={handleConvertClick}>Copy</button>
+				</div>
+			</div>
+		</div>
+  	);
+}
--- app/src/main.jsx
+++ app/src/main.jsx
@@ -34,32 +34,27 @@ export default function App() {
     };
 
     return (
-        <div className={`bg-${isDarkMode ? 'black' : 'gray-100'} text-${isDarkMode ? 'white' : 'gray-800'} font-sans transition-colors`}>
+        <div className={`bg-${isDarkMode ? 'git-brown' : 'git-brown'} text-${isDarkMode ? 'white' : 'git-brown'} font-sans transition-colors`}>
             <div className='max-w-4xl mx-auto px-4'>
                 <div className='flex flex-col items-center justify-center min-h-screen'>
-                    <header className='flex flex-col sm:flex-row sm:justify-between w-full pt-4 pb-8 px-2'>
+                    <header className='flex flex-col sm:flex-row sm:justify-center w-full pt-4 pb-8 px-2'>
                         <a className='flex flex-col' href='/'>
-                            <h1 className='font-mono sm:text-xl tracking-tight'>Prompt2Git</h1>
-                            <p className='font-mono font-bold text-gray-600'>Convierte tus prompts a lenguaje Git</p>
+                            <h1 className='flex font-sans font-bold sm:text-xl tracking-tight justify-center'>Prompt2Git</h1>
+                            <p className='flex font-sans font-bold justify-center'>Convierte tus prompts a git</p>
                         </a>
-                        <div className='flex items-center gap-3 pt-4'>
-                            <a className='rounded-full text-gray-500' href=''>
-                                <div className='flex items-center justify-center space-x-2 py-2 px-6 rounded-full bg-gray-50 text-black shadow-xl text-sm font-medium'>
-                                    <img src='public/github.svg' alt='Git' className='w-5 h-5' id='iconGit'/>
-                                    <p>Star on Github</p>
-                                </div>
-                            </a>
-                            <a className='flex items-center justify-center space-x-2 py-2 px-6 rounded-full text-black text-sm font-medium transition bg-gray-50 shadow-xl' href=''>
-                                <div className='group'>
-                                    <img src='public/coffee.svg' alt='Coffee' className='w-4 h-4'/>
-                                </div>
-                            </a>
-                        </div>
                     </header>
-                    <button className='absolute top-2.5 right-2.5 text-gray-500 focus:outline-none hover:scale-125 transition w-4 h-4' id='btn_version' onClick={handleVersionChange}>
-                        <img class='w-4 h-4' src='public/moon.svg' id='iconDark'></img>
-                    </button>
-                    <div className='flex flex-col md:flex-row w-full gap-6 bg-[#EEEEEE] rounded-2xl p-2'>
+                    <div>
+                        <button className={`flex items-center justify-center absolute top-2.5 right-4 text-gray-500 focus:outline-none hover:scale-125 transition w-8 h-8 bg-${isDarkMode ? 'git-white' : 'git-orange'} rounded-full`} id='btn_version' onClick={handleVersionChange}>
+                            <img class='w-4 h-4' src={`${isDarkMode ? 'public/sun.svg' : 'public/moon.svg'}`} id='iconMode'></img>
+                        </button>
+                        <button className='flex items-center justify-center absolute top-2.5 right-16 text-gray-500 focus:outline-none hover:scale-125 transition w-8 h-8 bg-git-orange rounded-full' id='btn_coffee'>
+                            <img class='w-4 h-4' src='public/coffee.svg' id='iconCoffee'></img>
+                        </button>
+                        <button className='flex items-center justify-center absolute top-2.5 right-28 text-gray-500 focus:outline-none hover:scale-125 transition w-8 h-8 bg-git-orange rounded-full' id='btn_github'>
+                            <img class='w-4 h-4' src='public/github.svg' id='iconGit'></img>
+                        </button>
+                    </div>
+                    <div className={`flex flex-col md:flex-row w-full gap-6 bg-${isDarkMode ? 'git-orange' : 'git-white-20'} rounded-2xl p-2`}>
                         <div className='w-full'>
                             <form className='rounded-xl bg-white container-w-gradient-border p-3 h-full w-full shadow-md'>
                                 <div className='flex flex-col h-full'>
@@ -77,8 +72,8 @@ export default function App() {
                         </div>
                         <div>
                             <div className='flex items-center md:h-full'>
-                                <button className='text-gray-700 cursor-pointer mx-auto'>
-                                    <img src='public/refresh.svg' alt='Switch' className='w-8 h-8 md:w-16 md:h-16' id='iconSwitch'></img>
+                                <button className='flex items-center justify-center  bg-blue-500 rounded-full cursor-pointer mx-auto h-8 w-8'>
+                                    <img src='public/switch.svg' alt='Switch' className='w-4 h-4 md:w-4 md:h-4' id='iconSwitch'></img>
                                 </button>
                             </div>
                         </div>
@@ -96,8 +91,8 @@ export default function App() {
                                                 </div>
                                             </button>
                                         </div>
-                                    </div>
-                                </form>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
